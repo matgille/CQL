586710e3c914840fd71c56a3cfffd14f83abe853
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2025-09-17 14:51:24 +0200

    Improve tests

--- tests/tests.py
+++ tests/tests.py
@@ -9,6 +9,12 @@ def import_test_queries(path):
 		list_of_queries = f.read().splitlines()
 	return [line.split("\t") for line in list_of_queries]
 
+def import_match_queries(path):
+	with open(path, "r") as f:
+		list_of_queries = f.read().splitlines()
+	as_splits = [line.split("\t") for line in list_of_queries]
+	return [(ast.literal_eval(nodes), query, GT) for nodes, query, GT in as_splits]
+
 
 class TestFunctions(unittest.TestCase):
 	def test_simple_match(self):
@@ -32,13 +38,17 @@ class TestQueries(unittest.TestCase):
 
 
 	def test_match_queries(self):
-		self.corpus = functions.import_corpus("tests/test_data/test_corpus.json")
-		self.queries = import_test_queries("tests/queries_match.txt")
+		self.queries = import_match_queries("tests/queries_match.txt")
 		self.MyEngine = functions.CQLEngine()
-		for query, GT in self.queries:
+		for idx, (nodes, query, GT) in enumerate(self.queries):
 			with self.subTest(query=query, GT=GT):
 				GT = True if GT == "True" else False
-				self.assertEqual(self.MyEngine.match(self.corpus, query, debug=False), GT)
+				match = self.MyEngine.match(nodes, query, debug=True)
+				self.assertEqual(match, GT,
+								 msg=f"\nTest {idx + 1} failed.\n"
+									 f"Query: {query}\n"
+									 f"Nodes: {nodes}\n"
+									 f"Match should be {GT}, is {match}")
 
 
 if __name__ == '__main__':
